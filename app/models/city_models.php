a494fb98a2cfcefb7a717dc0b3547d510877b252
Gabriel Rotger <rotgergabriel@gmail.com>
2026-02-16 11:26:59 +0100

    feat: add initial structure for city_models (WIP)

---
+++ app/models/city_models.php
@@ -0,0 +1,66 @@
+<?php
+
+$cities = [
+    'milano' => [
+        'hero' => [
+            'title' => 'Milano',
+            'image' => 'milano-hero.jpg'
+        ],
+
+        'social_media' => [
+            'facebook' => '...!',
+            'instagram' => '...!',
+            'whatsapp' => '...!',
+            'phone' => '...!',
+            'maps' => '...!'
+        ],
+
+        'info_box' => [
+            [
+                'label'   => 'Luogo di predicazione',
+                'content' => 'Via delle Camelie, 19, 20095 Cusano Milanino MI'
+            ],
+            [
+                'label'   => 'Servizio di culto',
+                'content' => 'Presenziale (Domenica 10:30 Devozionale 11:00 Scuola Domenicale)'
+            ],
+            [
+                'label'   => 'Pastore',
+                'content' => 'Jair Pedrozo Rinaldy'
+            ]
+        ],
+
+        'services' => [
+            'Escuela Dominical',
+            'Células en Hogares',
+            'Acción Social',
+            'Jóvenes'
+        ],
+
+        'board' => [
+            'photo' => 'pastor-bio.jpg',
+            'description' => 'Breve biografía o mensaje del pastor...'
+        ],
+
+        'location' => [
+            'text' => 'Encuéntranos en el corazón de Milán.',
+            'map_iframe' => 'https://www.google.com/maps/embed?pb=...'
+        ],
+
+        'pastor_family' => [
+            'photo' => 'pastor_milano_family.jpg',
+            'title' => 'Nuestros Pastores',
+            'description' => 'El pastor Juan Pérez junto a su esposa e hijos, sirviendo con amor en la ciudad de Milano desde hace 10 años.'
+        ],
+
+        'gallery' => [
+            'img1.jpg',
+            'img2.jpg',
+            'img3.jpg',
+            'img4.jpg'
+        ]
+    ]
+];
+
+return $cities;
+?>
